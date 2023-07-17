3bffe2233bed4dee2466f8f4f905ea7a6f5ad95c
NELSON IZQUIERDO <nelson.izquierdo@globalservs.com>
2023-07-17 16:00:33 -0400

    🆕 feat: persist state in sessionStorage

--- pruebas/01-reading-list/nedilio/src/app/hooks/useBooks.ts
+++ pruebas/01-reading-list/nedilio/src/app/hooks/useBooks.ts
@@ -1,20 +1,20 @@
 ﻿import { useEffect } from "react";
-import { useBooksStore } from "../store/books";
+import { useBooksStore, useReadingListStore } from "../store/books";
 import { Book, Library } from "../lib/types";
 
 function useBooks() {
   const books = useBooksStore((state) => state.library);
-  const readingList = useBooksStore((state) => state.readingList);
+  const readingList = useReadingListStore((state) => state.readingList);
   const fetchBooks = useBooksStore((state) => state.fetchBooks);
 
   const removeBookFromLibrary = useBooksStore(
     (state) => state.removeBookFromLibrary
   );
   const addBookToLibrary = useBooksStore((state) => state.addBookToLibrary);
-  const addBookToReadingList = useBooksStore(
+  const addBookToReadingList = useReadingListStore(
     (state) => state.addBookToReadingList
   );
-  const removeBookFromReadingList = useBooksStore(
+  const removeBookFromReadingList = useReadingListStore(
     (state) => state.removeBookFromReadingList
   );
 
@@ -31,12 +31,23 @@ function useBooks() {
   };
   useEffect(() => {
     (async function () {
-      //   const libraryLocalStorage = window.localStorage.getItem("library");
-      //   console.log(libraryLocalStorage);
-      const response = await fetch(`/books.json`);
-      const { library } = (await response.json()) as Library;
-      const books = library.map(({ book }) => book as Book);
-      fetchBooks(books);
+      const libraryLocalStorage =
+        window.sessionStorage.getItem("library-storage");
+      const readingListLocalStorage = window.sessionStorage.getItem(
+        "readingList-storage"
+      );
+
+      if (libraryLocalStorage && readingListLocalStorage) {
+        const { library } = JSON.parse(libraryLocalStorage).state;
+        const { readingList } = JSON.parse(readingListLocalStorage).state;
+
+        if (library.length === 0 && readingList.length === 0) {
+          const response = await fetch(`/books.json`);
+          const { library } = (await response.json()) as Library;
+          const books = library.map(({ book }) => book as Book);
+          fetchBooks(books);
+        }
+      }
     })();
   }, [fetchBooks]);
 
--- pruebas/01-reading-list/nedilio/src/app/store/books.ts
+++ pruebas/01-reading-list/nedilio/src/app/store/books.ts
@@ -1,30 +1,53 @@
 ﻿import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 import { Book } from "../lib/types";
 
 interface BooksState {
   library: Book[];
-  readingList: Book[];
   fetchBooks: (books: Book[]) => void;
   removeBookFromLibrary: (id: string) => void;
   addBookToLibrary: (book: Book) => void;
+}
+
+interface ReadingListState {
+  readingList: Book[];
   addBookToReadingList: (book: Book) => void;
   removeBookFromReadingList: (id: string) => void;
 }
 
-export const useBooksStore = create<BooksState>()((set) => ({
-  library: [],
-  readingList: [],
-  fetchBooks: async (books) => set({ library: books }),
-  removeBookFromLibrary: (id: string) =>
-    set((state) => ({
-      library: state.library.filter((book) => book.ISBN !== id),
-    })),
-  addBookToLibrary: (book: Book) =>
-    set((state) => ({ library: [...state.library, book] })),
-  addBookToReadingList: (book: Book) =>
-    set((state) => ({ readingList: [...state.readingList, book] })),
-  removeBookFromReadingList: (id: string) =>
-    set((state) => ({
-      readingList: state.readingList.filter((book) => book.ISBN !== id),
-    })),
-}));
+export const useBooksStore = create<BooksState>()(
+  persist(
+    (set, get) => ({
+      library: [],
+      fetchBooks: async (books) => set({ library: books }),
+      removeBookFromLibrary: (id: string) =>
+        set((state) => ({
+          library: state.library.filter((book) => book.ISBN !== id),
+        })),
+      addBookToLibrary: (book: Book) =>
+        set((state) => ({ library: [...state.library, book] })),
+    }),
+    {
+      name: "library-storage", // name of the item in the storage (must be unique)
+      storage: createJSONStorage(() => sessionStorage), // (optional) by default, 'localStorage' is used
+    }
+  )
+);
+
+export const useReadingListStore = create<ReadingListState>()(
+  persist(
+    (set, get) => ({
+      readingList: [],
+      addBookToReadingList: (book: Book) =>
+        set((state) => ({ readingList: [...state.readingList, book] })),
+      removeBookFromReadingList: (id: string) =>
+        set((state) => ({
+          readingList: state.readingList.filter((book) => book.ISBN !== id),
+        })),
+    }),
+    {
+      name: "readingList-storage", // name of the item in the storage (must be unique)
+      storage: createJSONStorage(() => sessionStorage), // (optional) by default, 'localStorage' is used
+    }
+  )
+);
